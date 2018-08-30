e79b78dae202ba1675c1c3427d43f17524224885
0xaeeeee <hkdjcmc@msn.cn>
2018-08-30 11:30:03 +0800

    create project

---
+++ MCHFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCHFM
+//
+//  Created by mc on 2018/8/30.
+//  Copyright © 2018年 immx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCHFM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MCHFM
+//
+//  Created by mc on 2018/8/30.
+//  Copyright © 2018年 immx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
